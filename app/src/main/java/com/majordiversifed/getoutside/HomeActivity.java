7803aca51133f4f3b6a6989f210c1c6e2aa13045
asuh9@gatech.edu <atsuh0124@gmail.com>
2016-02-20 06:17:15 -0500

    Disk persistence works now

--- app/src/main/java/com/majordiversifed/getoutside/HomeActivity.java
+++ app/src/main/java/com/majordiversifed/getoutside/HomeActivity.java
@@ -15,6 +15,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Firebase.setAndroidContext(this);
+        Firebase.getDefaultConfig().setPersistenceEnabled(true);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
